4fd53544ad18eb2970038edbbc83dec882b9fe13
Ivan Sabater Dominguez <45534652+ivansabater@users.noreply.github.com>
2024-11-14 14:33:43 +0100

    Clean Code functions

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -36,4 +36,16 @@
     console.log("Crear actor", birthdate);
     return true;
   }
+
+  const getPayAmount = ({
+    isDead = false,
+    isSeparated = true,
+    isRetired = false,
+  }) => {
+    if (isDead) return 1500;
+
+    if (isSeparated) return 2500;
+
+    return isRetired ? 3000 : 4000;
+  };
 })();
