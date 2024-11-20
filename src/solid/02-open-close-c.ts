9fcc962cdfa31a60b94f8ae3c59d767df7d93458
Ivan Sabater Dominguez <45534652+ivansabater@users.noreply.github.com>
2024-11-20 13:59:12 +0100

    Open and Close

---
+++ src/solid/02-open-close-c.ts
@@ -0,0 +1,15 @@
+// Quitar la dependencia de axios y usar fetch
+// import axios from "axios";
+
+export class HttpClient {
+  //   async get(url: string) {
+  //     const { data, status } = await axios.get(url);
+  //     return { data, status };
+  //   }
+  async get(url: string) {
+    const resp = await fetch(url);
+    const data = await resp.json();
+
+    return { data, status: resp.status };
+  }
+}
